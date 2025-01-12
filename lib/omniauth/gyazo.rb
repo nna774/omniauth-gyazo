3d567a22126ef1a68752fba98a6cbf23044237d3
Kugayama Nana <nna@nna774.net>
2025-01-13 01:29:05 +0900

    init commit

---
+++ lib/omniauth/gyazo.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gyazo/version"
+
+module Omniauth
+  module Gyazo
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/gyazo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::Gyazo do
+  it "has a version number" do
+    expect(Omniauth::Gyazo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
